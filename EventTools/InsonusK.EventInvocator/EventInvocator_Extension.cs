6d11b9effa46e392c9cecc781acbefff77f978de
Insonus.K <insonus.K@gmail.com>
2019-10-13 21:50:04 +0300

    Change extension to Delegates

--- EventTools/InsonusK.EventInvocator/EventInvocator_Extension.cs
+++ EventTools/InsonusK.EventInvocator/EventInvocator_Extension.cs
@@ -4,21 +4,37 @@ namespace InsonusK.EventInvocator
 {
     public static class EventInvocator_Extension
     {
+        /// <summary>
+        /// Safe invoke of event
+        /// </summary>
+        /// <param name="ev">event</param>
+        /// <param name="sender">sender</param>
+        /// <param name="args">event arguments</param>
+        /// <typeparam name="TEventArgs">Type of event arguments</typeparam>
         public static void SafeInvoke<TEventArgs>(this EventHandler<TEventArgs> ev, object sender, TEventArgs args)
         {
             EventHandler<TEventArgs> _ev = ev;
             _ev?.Invoke(sender,args);
         }
-
+        /// <summary>
+        /// Safe invoke of event
+        /// </summary>
+        /// <param name="ev">event</param>
+        /// <param name="sender">sender</param>
         public static void SafeInvoke(this EventHandler ev, object sender)
         {
             EventHandler _ev = ev;
             _ev?.Invoke(sender,null);
         }
 
-        public static void SafeInvoke<TAction>(TAction _delegate,object[] parameters = null) where TAction:Delegate
+        /// <summary>
+        /// Safe invoke of delegate
+        /// </summary>
+        /// <param name="delegate">Delegate</param>
+        /// <param name="parameters">delegate parameters</param>
+        public static void SafeInvoke(this Delegate @delegate, object[] parameters = null) 
         {
-            TAction _ev = _delegate;
+            Delegate _ev = @delegate;
             _ev?.DynamicInvoke(parameters);
         }
     }
